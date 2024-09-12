76d5f4dadff8786e1aa5c8cb651a65b997e19827
--unset <vaibhav998819@gmail.com>
2024-09-13 03:13:05 +0530

    component update

--- Components/Body.js
+++ Components/Body.js
@@ -2,14 +2,26 @@ import ProdData from "./prod_data_cont";
 import Top_container from "./Top_container";
 import ProdData from "./prod_data_cont";
 import WhyGelato from "./why_gelato";
+import DemandSelling from "./demand_selling";
+import contact_white_img from "../public/contact_img.svg"
 const Body=()=>{
   return(
+    <div>
     <div>
     <Top_container/>
     <ProdData/>
     
     <h1 style={{fontSize:"80px",textAlign:"center",fontFamily:"Helvetica Neue"}}>Why choose Gelato</h1>
     <WhyGelato/>
+    
+    <DemandSelling/>
+    </div>
+    
+
+    <div class="floating-text">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7zUTEpNU1WndlpmAlVUWMcymjlXF-YnVaNR2ksIKYFPrtbzwHo21WjKvzg0pBDL3LKKY&usqp=CAU" style={{width:"40px",height:"40px",padding:"10px"}}/>
+            <h2>  Contact us</h2>
+    </div>
     </div>
   );
 }
---
+++ Components/demand_selling.js
@@ -0,0 +1,21 @@
+const DemandSelling=()=>{
+    return(
+    <div className="demand_sellng "style={{  backgroundColor: "rgb(251,246,239)"
+    }} >
+      <div className="demand_sellng-cont-left" style={{marginLeft:"250px ",fontFamily:"GelatoSans-Medium, sans-serif"}}>
+        <h5 style={{fontSize:"30px"}}>For print on demand sellers</h5>
+        <h1 style={{fontSize:"80px"}}>Accelerate business growth with innovative design tools and apps</h1>
+        <h5 style={{fontSize:"30px"}}>See how our cutting-edge solutions can help you reach new customers and maximize your profits.</h5>
+        
+        <h3 style={{backgroundColor:"black",color:"white",width:"110px",padding :"15px",borderRadius:"30px",alignContent:"center",justifyContent:"centre"}}>Get Started</h3>
+      
+      </div>
+      
+      <div className="demand_sellng-cont-right" style={{marginRight:"200px ",justifyContent:"center"}}>
+        <img src="https://a.storyblok.com/f/165154/1200x1200/35ff04c4c9/cc-55_marketing-assets-for-personalization-studio.gif/m/" style={{width:"750px",height:"600px"}}/>
+      </div>
+    </div>
+    );
+
+}
+export default DemandSelling;
